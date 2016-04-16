8f62d920570a4ec39e9ce692640694270f288e20
Pascal Hartig <phartig@rdrei.net>
2016-04-16 13:26:43 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,13 @@
+# Pux Counter Example
+
+The canonical [Elm StartApp Example](https://github.com/evancz/start-app) in
+[PureScript](http://www.purescript.org), ready to be run with
+[pulp](https://github.com/bodil/pulp).
+
+## Running
+
+```
+$ bower install & npm install
+$ pulp server
+$ open http://localhost:1337/
+```
---
+++ src/Main.purs
@@ -0,0 +1,36 @@
+module Main where
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Prelude (Unit, bind, const, ($), show, (-), (+))
+import Pux (renderToDOM, fromSimple, start)
+import Pux.Html (Html, text, button, span, div)
+import Pux.Html.Events (onClick)
+import Signal.Channel (CHANNEL)
+
+data Action = Increment | Decrement
+
+type State = Int
+
+update :: Action -> State -> State
+update Increment = (_ + 1)
+update Decrement = (_ - 1)
+
+view :: State -> Html Action
+view count =
+  div
+    []
+    [ button [ onClick $ const Decrement ] [ text "(-)" ]
+    , span [] [ text (show count) ]
+    , button [ onClick $ const Increment ] [ text "(+)" ]
+    ]
+
+main :: forall eff. Eff (err :: EXCEPTION, channel :: CHANNEL | eff) Unit
+main = do
+  app <- start
+    { initialState: 0
+    , update: fromSimple update
+    , view: view
+    , inputs: []
+    }
+  renderToDOM "#app" app.html
